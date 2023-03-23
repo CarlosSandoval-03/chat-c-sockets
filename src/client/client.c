417d57c32eb13288ee2438522a52962a17ebf1fa
Carlos Sandoval <csandovalc@unal.edu.co>
2023-03-23 00:19:51 -0500

    feat(client): configure and start client

--- src/client/client.c
+++ src/client/client.c
@@ -1,13 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-#include <sys/wait.h>
+
 #include <sys/types.h>
 #include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
 
-int main() {
+#include "./client.h"
+#include "../socket/socket.h"
+#include "../server/server.h"
+
+int main(int argc, char *argv[]) {
+  int client_fd = create_socket();
+
+  char *host = HOST;
+  unsigned int port = 3535;
+  if (argc >= 2) {
+    host = argv[1];
+  }
+  if (argc >= 3) {
+    char *end_ptr;
+    port = (unsigned int) strtol(argv[2], end_ptr, 10);
+  }
+
+  socket_connect_client(client_fd, host, port);
+
+  char buffer[1024];
+  ssize_t recv_bytes = recv(client_fd, buffer, 1024, 0);
+  if (recv_bytes < 1024) {
+    perror("RECV: AN ERROR HAS BEEN PRESENTED WHEN RECEIVING THE COMMUNICATION");
+    exit(EXIT_FAILURE);
+  }
+
+  buffer[recv_bytes] = '\0';
+  printf("\nMensaje: %s", buffer);
+
+  close(client_fd);
   return EXIT_SUCCESS;
 }
---
+++ src/client/client.h
@@ -0,0 +1,5 @@
+#pragma once
+#ifndef _CLIENT_H_
+#define _CLIENT_H_
+
+#endif // !_CLIENT_H_
